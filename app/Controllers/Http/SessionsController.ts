10667335fa5e7a7bcd9e6be4d47b8dfffac60474
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-12 15:05:49 -0400

    feat: Ajustes na rota de login

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -5,14 +5,10 @@ export default class SessionsController {
   public async store(ctx: HttpContextContract) {
     await ctx.request.validate(SessionValidator)
 
-    // await Redis.set('foo', 'bar')
-    // const value = await Redis.get('foo')
-    // console.log(value)
-
     const { email, password } = ctx.request.all()
 
     try {
-      const token = await ctx.auth.use('api').attempt(email, password)
+      const token = await ctx.auth.attempt(email, password)
       return token
     } catch (err) {
       console.log(err)
