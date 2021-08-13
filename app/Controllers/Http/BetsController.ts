4d13f718e036015e679aa278d0089bc63a708be1
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-13 17:03:47 -0400

    feat: create Bet controller and validator

---
+++ app/Controllers/Http/BetsController.ts
@@ -0,0 +1,11 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class BetsController {
+  public async store({ request, response }: HttpContextContract) {}
+
+  public async show({ request, response }: HttpContextContract) {}
+
+  public async update({ request, response }: HttpContextContract) {}
+
+  public async destroy({ request, response }: HttpContextContract) {}
+}
