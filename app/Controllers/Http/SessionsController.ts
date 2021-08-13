1eb507b0d4d159989ab6dd2aebe08fe4b9214d7d
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-08-13 12:15:43 -0400

    feat: devolvendo os dados do usuário no login

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -1,6 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import SessionValidator from 'App/Validators/SessionValidator'
-// import Redis from '@ioc:Adonis/Addons/Redis'
+import User from 'App/Models/User'
 export default class SessionsController {
   public async store(ctx: HttpContextContract) {
     await ctx.request.validate(SessionValidator)
@@ -8,12 +8,24 @@ export default class SessionsController {
     const { email, password } = ctx.request.all()
 
     try {
-      const token = await ctx.auth.attempt(email, password)
-      return token
+      const authData = await ctx.auth.attempt(email, password)
+      const user = await User.findByOrFail('email', email)
+      const userJSON = user.serialize({
+        fields: {
+          pick: ['id', 'name', 'email', 'is_admin'],
+        },
+      })
+
+      return {
+        user: userJSON,
+        token: authData.token,
+      }
     } catch (err) {
       console.log(err)
 
-      return ctx.response.badRequest('Invalid credentials')
+      return ctx.response.badRequest({
+        error: { message: 'Invalid credentials' },
+      })
     }
   }
 }
