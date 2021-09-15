58834b8de11398bf8623a7b639fec4b29e4a3a01
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-15 16:13:54 -0400

    test: Creation of Mega Sena bet creation test case

---
+++ test/bet.spec.ts
@@ -0,0 +1,59 @@
+import supertest from 'supertest'
+import test from 'japa'
+import url from '../base_url'
+import User from 'App/Models/User'
+import Type from 'App/Models/Type'
+
+test.group('Types tests', (group) => {
+  let AUTH_TOKEN: string
+
+  group.before(async () => {
+    await User.create({
+      name: 'User Bet Teste',
+      email: 'user_bet_teste@teste.com',
+      password: '123456',
+    })
+
+    await Type.firstOrCreate({
+      type: 'Mega-Sena',
+      description:
+        'Escolha 6 números dos 60 disponíveis na mega-sena. Ganhe com 6, 5 ou 4 acertos. São realizados dois sorteios semanais para você apostar e torcer para ficar milionário.',
+      range: 60,
+      price: 4.5,
+      maxNumber: 6,
+      color: '#01AC66',
+      minCartValue: 30,
+    })
+
+    const response = await supertest(url)
+      .post('/sessions')
+      .send({ email: 'user_bet_teste@teste.com', password: '123456' })
+
+    AUTH_TOKEN = response.body.token
+  })
+
+  test('Creating a mega sena bet with random numbers', async () => {
+    const megaSena = await Type.findBy('type', 'Mega-Sena')
+    const numbers: Array<number> = []
+
+    while (numbers.length < megaSena!.maxNumber) {
+      const possibleNumber = Math.ceil(Math.random() * megaSena!.range)
+
+      if (!numbers.some((selectedNumber) => possibleNumber === selectedNumber)) {
+        numbers.push(possibleNumber)
+      }
+    }
+
+    const newBet = {
+      numbers,
+      price: megaSena!.price,
+      typeId: megaSena!.id,
+    }
+
+    await supertest(url)
+      .post('/bets')
+      .set('Authorization', 'bearer ' + AUTH_TOKEN)
+      .send({ bets: [newBet] })
+      .expect(200)
+  })
+})
