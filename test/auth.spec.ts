97ab8dd2865bb1430bbd856ae5ef089312896ace
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-15 13:47:46 -0400

    feat: login test

---
+++ test/auth.spec.ts
@@ -0,0 +1,28 @@
+import User from 'App/Models/User'
+import test from 'japa'
+import supertest from 'supertest'
+import Logger from '@ioc:Adonis/Core/Logger'
+import url from '../base_url'
+
+test.group('Auth tests', (group) => {
+  group.before(async () => {
+    await User.create({
+      name: 'User Teste',
+      email: 'user_teste@teste.com',
+      password: '123456',
+    })
+  })
+
+  test('Must return user information and token when valid credentials are sent', async (assert) => {
+    const userCredentials = {
+      email: 'user_teste@teste.com',
+      password: '123456',
+    }
+
+    const result = await supertest(url).post('/sessions').send(userCredentials)
+
+    Logger.info('Response body:\n%o', result.body)
+
+    assert.hasAllKeys(result.body, ['email', 'id', 'name', 'token', 'is_admin'])
+  })
+})
