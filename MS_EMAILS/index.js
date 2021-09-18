b3a4b1165ebe8e40c30a4db0e4c7c0d46a493ae1
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-09-17 20:23:21 -0400

    Configuranda Kafta e testandos os primeiros consumers e producers

--- MS_EMAILS/index.js
+++ MS_EMAILS/index.js
@@ -1,11 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const { Kafka } = require("kafkajs");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:29092"],
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get("/send", async (req, res) => {
+  const producer = kafka.producer();
+
+  await producer.connect();
+  await producer.send({
+    topic: "new-bet",
+    messages: [{ value: "Hello KafkaJS user!" }],
+  });
+
+  await producer.disconnect();
+
+  res.send("Hello World!");
+});
+
+app.get("/start_receiver", async (req, res) => {
+  const consumer = kafka.consumer({ groupId: "test-group" });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "new-bet", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      });
+    },
+  });
+});
+
+app.listen(port, async () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+
+  const consumer = kafka.consumer({ groupId: "test-group" });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "new-bet", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      });
+    },
+  });
+});
